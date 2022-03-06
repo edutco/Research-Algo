a9b766af1cf1a8de6fe9448b8325d654575e85dc
Edut Cohen <57194044+edutco@users.noreply.github.com>
2022-03-06 19:18:37 +0200

    Ex 2 files

---
+++ myfile.py
@@ -0,0 +1,28 @@
+"""
+this is the file general ducumantation
+"""
+
+def func1(param1: int, param2: dict[str,int]):
+    """
+    This function has two parameters
+    """
+    print("Hello")
+
+
+def func2():
+    """
+    This function has no parameters and a return value.
+    """
+    return [1,2,3]
+
+def func3(args: str)->int:
+    """
+    function number 3
+    """
+    return 0 
+
+def func4(arg1, arg2)->str:
+    """
+    this is the fourth function
+    """
+    return "great!"

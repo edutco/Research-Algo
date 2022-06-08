a48cf88cb9f38486a4df904931d09516a379163c
Edut Cohen <57194044+edutco@users.noreply.github.com>
2022-06-08 13:13:14 +0300

    Update __init__.py

--- Ex11/__init__.py
+++ Ex11/__init__.py
@@ -5,7 +5,6 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ecf6e975838a2f7bf3c5dbe7d55ebe5b'  ###
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
 
 
-from Ex11 import *
+from . import forms
